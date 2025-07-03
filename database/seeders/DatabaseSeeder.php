fc87747b50f45f65b189c47c4b68c0be0986ae25
jeremie <jeremie-codes>
2025-07-03 11:32:17 +0100

    meme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,17 +12,17 @@ class DatabaseSeeder extends Seeder
             // Seeders de base (rôles, permissions, fonctions)
             RolePermissionSeeder::class,
             FonctionSeeder::class,
-            
+
             // Seeders géographiques (pays, villes, communes)
             LocationSeeder::class,
-            
+
             // Seeders des entités principales
-            SiteSeeder::class,
-            PoolSeeder::class,
-            
+            // SiteSeeder::class,
+            // PoolSeeder::class,
+
             // Seeders des membres et utilisateurs
             MemberSeeder::class,
-            UserSeeder::class,
+            // UserSeeder::class,
         ]);
     }
 }
--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -1,115 +1,190 @@
 <?php
 
+// namespace Database\Seeders;
+
+// use Illuminate\Database\Seeder;
+// use App\Models\Member;
+// use App\Models\Site;
+// use App\Models\Pool;
+// use App\Models\Fonction;
+// use App\Models\City;
+// use App\Models\Township;
+// use Faker\Factory as Faker;
+
+// class MemberSeeder extends Seeder
+// {
+//     public function run()
+//     {
+//         $faker = Faker::create('fr_FR');
+
+//         // Récupérer les données nécessaires
+//         $sites = Site::all();
+//         $pools = Pool::all();
+//         $fonctions = Fonction::all();
+//         $cities = City::all();
+//         $townships = Township::all();
+
+//         if ($sites->isEmpty()) {
+//             $this->command->error('Aucun site trouvé. Veuillez d\'abord exécuter le SiteSeeder.');
+//             return;
+//         }
+
+//         $this->command->info('Création de membres avec données géographiques...');
+
+//         // Créer 5 membres avec des données réalistes
+//         for ($i = 1; $i <= 20; $i++) {
+//             // Sélectionner aléatoirement une ville et une commune
+//             $city = $cities->random();
+//             $cityTownships = $townships->where('city_id', $city->id);
+//             $township = $cityTownships->isNotEmpty() ? $cityTownships->random() : null;
+
+//             // Sélectionner un site aléatoire
+//             $site = $sites->random();
+
+//             // Sélectionner un pool aléatoire (optionnel)
+//             $pool = $pools->isNotEmpty() && $faker->boolean(70) ? $pools->random() : null;
+
+//             // Sélectionner une fonction aléatoire (optionnel)
+//             $fonction = $fonctions->isNotEmpty() && $faker->boolean(80) ? $fonctions->random() : null;
+
+//             // Générer des noms réalistes congolais
+//             $firstNames = [
+//                 'Jean', 'Marie', 'Pierre', 'Paul', 'Joseph', 'Emmanuel', 'Grace', 'Patience',
+//                 'Espoir', 'Joie', 'Paix', 'Amour', 'Fidèle', 'Béni', 'Gloire', 'Victoire',
+//                 'Merci', 'Chance', 'Bonheur', 'Lumière', 'Sagesse', 'Force', 'Courage',
+//                 'Mukendi', 'Kabongo', 'Tshimanga', 'Mbuyi', 'Ngoy', 'Kalala', 'Ilunga',
+//                 'Kasongo', 'Mwamba', 'Katanga', 'Luboya', 'Mujinga', 'Kapinga', 'Mulamba'
+//             ];
+
+//             $lastNames = [
+//                 'Kabila', 'Tshisekedi', 'Katumbi', 'Bemba', 'Kamerhe', 'Muzito', 'Ruberwa',
+//                 'Mende', 'Lumumba', 'Kasavubu', 'Mobutu', 'Mulele', 'Gizenga', 'Kamitatu',
+//                 'Mukendi', 'Kabongo', 'Tshimanga', 'Mbuyi', 'Ngoy', 'Kalala', 'Ilunga',
+//                 'Kasongo', 'Mwamba', 'Katanga', 'Luboya', 'Mujinga', 'Kapinga', 'Mulamba',
+//                 'Ndala', 'Mbala', 'Nkulu', 'Mpiana', 'Wemba', 'Koffi', 'Fally', 'Werrason'
+//             ];
+
+//             $middleNames = [
+//                 'wa', 'bin', 'ben', 'de', 'du', 'le', 'la', 'van', 'von', 'Mac', 'Mc',
+//                 'Mwana', 'Muana', 'Bana', 'Tatu', 'Nkashama', 'Ngalula', 'Mpiana'
+//             ];
+
+//             $genders = ['M', 'F'];
+
+//             $firstName = $faker->randomElement($firstNames);
+//             $lastName = $faker->randomElement($lastNames);
+//             $middleName = $faker->boolean(60) ? $faker->randomElement($middleNames) : null;
+
+//             // Générer un numéro de téléphone congolais
+//             $phoneFormats = [
+//                 '+243 9' . $faker->numerify('## ### ###'),
+//                 '+243 8' . $faker->numerify('## ### ###'),
+//                 '+243 99' . $faker->numerify('# ### ###'),
+//                 '+243 97' . $faker->numerify('# ### ###'),
+//                 '+243 81' . $faker->numerify('# ### ###'),
+//                 '+243 82' . $faker->numerify('# ### ###'),
+//                 '+243 83' . $faker->numerify('# ### ###'),
+//                 '+243 84' . $faker->numerify('# ### ###'),
+//                 '+243 85' . $faker->numerify('# ### ###'),
+//                 '+243 89' . $faker->numerify('# ### ###'),
+//             ];
+
+//             $member = Member::create([
+//                 'firstname' => $firstName,
+//                 'lastname' => $lastName,
+//                 'middlename' => $middleName,
+//                 'phone' => $faker->randomElement($phoneFormats),
+//                 'gender' => $faker->randomElement($genders),
+//                 'site_id' => $site->id,
+//                 'city_id' => $city->id,
+//                 'township_id' => $township ? $township->id : null,
+//                 'pool_id' => $pool ? $pool->id : null,
+//                 'libelle_pool' => !$pool && $faker->boolean(30) ? $faker->words(2, true) : null,
+//                 'fonction_id' => $fonction ? $fonction->id : null,
+//                 'is_active' => $faker->boolean(90), // 90% de chance d'être actif
+//                 'created_at' => $faker->dateTimeBetween('-2 years', 'now'),
+//                 'updated_at' => now(),
+//             ]);
+
+//             // Le numéro d'adhésion sera généré automatiquement par le modèle
+//             $this->command->info("Membre créé: {$member->full_name} - {$member->membershipNumber} ({$city->name}, {$site->name})");
+//         }
+
+//         $this->command->info('50 membres créés avec succès !');
+//     }
+// }
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Member;
 use App\Models\Site;
 use App\Models\Pool;
+use App\Models\Member;
 use App\Models\Fonction;
-use App\Models\City;
-use App\Models\Township;
-use Faker\Factory as Faker;
+use Illuminate\Support\Str;
 
-class MemberSeeder extends Seeder
+class DaipinSeeder extends Seeder
 {
     public function run()
     {
-        $faker = Faker::create('fr_FR');
+        // Créer le site DAIPIN
+        $site = Site::firstOrCreate([
+            'code' => 'DPN'
+        ], [
+            'name' => 'DAIPIN',
+            'location' => 'Localisation à définir',
+            'is_active' => true,
+        ]);
 
-        // Récupérer les données nécessaires
-        $sites = Site::all();
-        $pools = Pool::all();
-        $fonctions = Fonction::all();
-        $cities = City::all();
-        $townships = Township::all();
+        // Récupérer la fonction "Chef de Pool"
+        $fonctionChef = Fonction::where('name', 'Chef de Pool')->first();
 
-        if ($sites->isEmpty()) {
-            $this->command->error('Aucun site trouvé. Veuillez d\'abord exécuter le SiteSeeder.');
-            return;
-        }
+        $poolsData = [
+            ['name' => 'Bambala', 'chefs' => ['Mr Junior', 'Mr Justin']],
+            ['name' => 'Secteur Agricole', 'chefs' => []],
+            ['name' => 'Makaku Mbwa', 'chefs' => ['Maman Zizi']],
+            ['name' => 'Poto Poto', 'chefs' => ['Mr Luc']],
+            ['name' => 'Ebiko I', 'chefs' => ['Mr Eli']],
+            ['name' => 'Ebiko II', 'chefs' => ['Mr Ardent']],
+            ['name' => 'Bukanga Lonzo I', 'chefs' => ['Mr Mbuta']],
+            ['name' => 'Bukanga Lonzo II', 'chefs' => []],
+            ['name' => 'Parc', 'chefs' => ['Maman Mado']],
+            ['name' => 'Likana I', 'chefs' => ['Mr Crispin']],
+            ['name' => 'Likana II', 'chefs' => ['Mr Andunga']],
+            ['name' => 'Vuku Vuku', 'chefs' => ['Mr Nool']],
+            ['name' => 'Pisciculture I', 'chefs' => ['Mme Mireille']],
+            ['name' => 'Pisciculture II', 'chefs' => []],
+            ['name' => 'Couvoirs', 'chefs' => ['Mr Olivier Ngesimi']],
+            ['name' => 'V.A.B', 'chefs' => ['Mr Alpha']],
+            ['name' => 'Antenne', 'chefs' => ['Mr Coralie']],
+            ['name' => 'Bambou', 'chefs' => ['Pélagie']],
+            ['name' => 'Mukuna', 'chefs' => ['Gorethie']],
+            ['name' => 'Jules', 'chefs' => ['Romain']],
+        ];
 
-        $this->command->info('Création de membres avec données géographiques...');
-
-        // Créer 5 membres avec des données réalistes
-        for ($i = 1; $i <= 20; $i++) {
-            // Sélectionner aléatoirement une ville et une commune
-            $city = $cities->random();
-            $cityTownships = $townships->where('city_id', $city->id);
-            $township = $cityTownships->isNotEmpty() ? $cityTownships->random() : null;
-
-            // Sélectionner un site aléatoire
-            $site = $sites->random();
-
-            // Sélectionner un pool aléatoire (optionnel)
-            $pool = $pools->isNotEmpty() && $faker->boolean(70) ? $pools->random() : null;
-
-            // Sélectionner une fonction aléatoire (optionnel)
-            $fonction = $fonctions->isNotEmpty() && $faker->boolean(80) ? $fonctions->random() : null;
-
-            // Générer des noms réalistes congolais
-            $firstNames = [
-                'Jean', 'Marie', 'Pierre', 'Paul', 'Joseph', 'Emmanuel', 'Grace', 'Patience',
-                'Espoir', 'Joie', 'Paix', 'Amour', 'Fidèle', 'Béni', 'Gloire', 'Victoire',
-                'Merci', 'Chance', 'Bonheur', 'Lumière', 'Sagesse', 'Force', 'Courage',
-                'Mukendi', 'Kabongo', 'Tshimanga', 'Mbuyi', 'Ngoy', 'Kalala', 'Ilunga',
-                'Kasongo', 'Mwamba', 'Katanga', 'Luboya', 'Mujinga', 'Kapinga', 'Mulamba'
-            ];
-
-            $lastNames = [
-                'Kabila', 'Tshisekedi', 'Katumbi', 'Bemba', 'Kamerhe', 'Muzito', 'Ruberwa',
-                'Mende', 'Lumumba', 'Kasavubu', 'Mobutu', 'Mulele', 'Gizenga', 'Kamitatu',
-                'Mukendi', 'Kabongo', 'Tshimanga', 'Mbuyi', 'Ngoy', 'Kalala', 'Ilunga',
-                'Kasongo', 'Mwamba', 'Katanga', 'Luboya', 'Mujinga', 'Kapinga', 'Mulamba',
-                'Ndala', 'Mbala', 'Nkulu', 'Mpiana', 'Wemba', 'Koffi', 'Fally', 'Werrason'
-            ];
-
-            $middleNames = [
-                'wa', 'bin', 'ben', 'de', 'du', 'le', 'la', 'van', 'von', 'Mac', 'Mc',
-                'Mwana', 'Muana', 'Bana', 'Tatu', 'Nkashama', 'Ngalula', 'Mpiana'
-            ];
-
-            $genders = ['M', 'F'];
-
-            $firstName = $faker->randomElement($firstNames);
-            $lastName = $faker->randomElement($lastNames);
-            $middleName = $faker->boolean(60) ? $faker->randomElement($middleNames) : null;
-
-            // Générer un numéro de téléphone congolais
-            $phoneFormats = [
-                '+243 9' . $faker->numerify('## ### ###'),
-                '+243 8' . $faker->numerify('## ### ###'),
-                '+243 99' . $faker->numerify('# ### ###'),
-                '+243 97' . $faker->numerify('# ### ###'),
-                '+243 81' . $faker->numerify('# ### ###'),
-                '+243 82' . $faker->numerify('# ### ###'),
-                '+243 83' . $faker->numerify('# ### ###'),
-                '+243 84' . $faker->numerify('# ### ###'),
-                '+243 85' . $faker->numerify('# ### ###'),
-                '+243 89' . $faker->numerify('# ### ###'),
-            ];
-
-            $member = Member::create([
-                'firstname' => $firstName,
-                'lastname' => $lastName,
-                'middlename' => $middleName,
-                'phone' => $faker->randomElement($phoneFormats),
-                'gender' => $faker->randomElement($genders),
+        foreach ($poolsData as $poolData) {
+            $pool = Pool::create([
                 'site_id' => $site->id,
-                'city_id' => $city->id,
-                'township_id' => $township ? $township->id : null,
-                'pool_id' => $pool ? $pool->id : null,
-                'libelle_pool' => !$pool && $faker->boolean(30) ? $faker->words(2, true) : null,
-                'fonction_id' => $fonction ? $fonction->id : null,
-                'is_active' => $faker->boolean(90), // 90% de chance d'être actif
-                'created_at' => $faker->dateTimeBetween('-2 years', 'now'),
-                'updated_at' => now(),
+                'name' => $poolData['name'],
+                'description' => null,
+                'is_active' => true
             ]);
 
-            // Le numéro d'adhésion sera généré automatiquement par le modèle
-            $this->command->info("Membre créé: {$member->full_name} - {$member->membershipNumber} ({$city->name}, {$site->name})");
+            foreach ($poolData['chefs'] as $chefName) {
+                Member::create([
+                    'firstname' => $chefName,
+                    'lastname' => null,
+                    'membershipNumber' => strtoupper('DPN-' . Str::random(5)),
+                    'site_id' => $site->id,
+                    'pool_id' => $pool->id,
+                    'libelle_pool' => null,
+                    'fonction_id' => $fonctionChef?->id,
+                    'is_active' => true,
+                    'date_adhesion' => now(),
+                ]);
+            }
         }
 
-        $this->command->info('50 membres créés avec succès !');
+        $this->command->info("✅ Pools et Chefs de Pool de DAIPIN créés avec succès.");
     }
 }
