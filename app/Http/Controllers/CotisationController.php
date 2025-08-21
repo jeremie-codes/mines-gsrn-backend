281e9c0cfd99bdc7cfc2dbf6a56ed4a69164d6cf
jeremie-codes <jrmmianda@gmail.com>
2025-08-21 17:04:42 +0100

    ddede

--- app/Http/Controllers/CotisationController.php
+++ app/Http/Controllers/CotisationController.php
@@ -539,7 +539,7 @@ class CotisationController extends Controller
 
             $transaction->update([
                 'status' => 'failed', 
-                'callback_response' => json_encode($data),
+                'callback_response' => json_encode($data->code),
             ]);
                 
             return response()->json([
